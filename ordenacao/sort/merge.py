932cfb24feb0626127815580f208045acaa252cf
Alysson Naves <anaves@gmail.com>
2023-03-08 21:43:01 -0300

    aula 8-3-2023

--- ordenacao/sort/merge.py
+++ ordenacao/sort/merge.py
@@ -1,6 +1,7 @@
 def intercala(lista, inicio, meio, fim):
     i = inicio
     j = meio
+
     aux = []
     while i<meio and j<fim:
         if lista[i]<lista[j]:
@@ -16,10 +17,25 @@ def intercala(lista, inicio, meio, fim):
     while j<fim:
         aux.append(lista[j])
         j+=1
-    return aux
+    print(lista)
+    print(aux)
+    #return aux
 
-L = [0,1,6,20,4,5,7,10]
+def merge_sort(inicio, fim, lista):
+    if inicio < fim-1:
+        meio = (inicio+fim)//2
+        print(f'{inicio}\t|{fim}\t|{fim-1}\t|{meio}',end='\t')
+        print(lista[inicio:fim])
+        merge_sort(inicio, meio, lista)
+        merge_sort(meio, fim, lista)
+        intercala(lista,inicio,meio,fim)
+    
+
+L = [0,1,6,20,4,5,7,10, 3, 2]
 print(L)
-L_O = intercala(L, 0, 4, 8)
-print(L_O)
+#print('inicio\t|fim\t|fim-1\t|meio')
+#merge_sort(0,len(L),L)
+#print(L)
+L_O = intercala(L, 0, 5, len(L))
+#print(L_O)
 # L_O = [0,1,4,5,6,7,10,20]
